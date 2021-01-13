9018e55b6702187bfe3a60ac60e4d8f71b6c9712
alexanderi96 <alessandro.ianne96@gmail.com>
2021-01-13 17:25:03 +0100

    Implementing minus assign (-=) divide assign (/=) and times assign (*=) operators

--- ast/divideassign.go
+++ ast/divideassign.go
@@ -16,6 +16,35 @@ func NewDivideAssign(l, r Node) Node {
 }
 
 func (d DivideAssign) Eval(env *obj.Env) obj.Object {
+	var name string
+	var left = d.l.Eval(env)
+	var right = d.r.Eval(env)
+
+	if ident, ok := d.l.(Identifier); ok {
+    	name = ident.String()
+	} else {
+    	return obj.NewError("cannot assign to literal")
+	}
+
+	if isError(left) {
+		return left
+	}
+	if isError(right) {
+		return right
+	}
+
+	if !assertTypes(left, obj.INT, obj.FLOAT) {
+		return obj.NewError("unsupported operator '/=' for type %v", left.Type())
+	}
+	if !assertTypes(right, obj.INT, obj.FLOAT) {
+		return obj.NewError("unsupported operator '/=' for type %v", right.Type())
+	}
+
+	left, right = toFloat(left, right)
+	l := left.(*obj.Float).Val()
+	r := right.(*obj.Float).Val()
+	env.Set(name, obj.NewFloat(l / r))
+
 	return obj.NullObj
 }
 
--- ast/minusassign.go
+++ ast/minusassign.go
@@ -16,6 +16,49 @@ func NewMinusAssign(l, r Node) Node {
 }
 
 func (m MinusAssign) Eval(env *obj.Env) obj.Object {
+	var name string
+	var left = m.l.Eval(env)
+	var right = m.r.Eval(env)
+
+	if ident, ok := m.l.(Identifier); ok {
+    	name = ident.String()
+	} else {
+    	return obj.NewError("cannot assign to literal")
+	}
+
+	if isError(left) {
+		return left
+	}
+	if isError(right) {
+		return right
+	}
+
+	if !assertTypes(left, obj.INT, obj.FLOAT) {
+		return obj.NewError("unsupported operator '-=' for type %v", left.Type())
+	}
+	if !assertTypes(right, obj.INT, obj.FLOAT) {
+		return obj.NewError("unsupported operator '-=' for type %v", right.Type())
+	}
+
+	switch {
+		case assertTypes(left, obj.INT) && assertTypes(right, obj.INT):
+			l := left.(*obj.Integer).Val()
+			r := right.(*obj.Integer).Val()
+			env.Set(name, obj.NewInteger(l - r))
+
+		case assertTypes(left, obj.FLOAT, obj.INT) && assertTypes(right, obj.FLOAT, obj.INT):
+			left, right = toFloat(left, right)
+			l := left.(*obj.Float).Val()
+			r := right.(*obj.Float).Val()
+			env.Set(name, obj.NewFloat(l - r))
+
+		default:
+			return obj.NewError(
+				"invalid operation %v -= %v (wrong types %v and %v)",
+				left, right, left.Type(), right.Type(),
+			)
+	}
+
 	return obj.NullObj
 }
 

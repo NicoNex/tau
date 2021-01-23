fcd9084e19d240fddfb606a0fb6267b0a1dd3539
Fefefo <tardiolifederico@gmail.com>
2021-01-23 03:31:58 +0100

    Fix output of %=

--- ast/modassign.go
+++ ast/modassign.go
@@ -34,10 +34,10 @@ func (p ModAssign) Eval(env *obj.Env) obj.Object {
 	}
 
 	if !assertTypes(left, obj.INT) {
-		return obj.NewError("unsupported operator '+=' for type %v", left.Type())
+		return obj.NewError("unsupported operator '%=' for type %v", left.Type())
 	}
 	if !assertTypes(right, obj.INT) {
-		return obj.NewError("unsupported operator '+=' for type %v", right.Type())
+		return obj.NewError("unsupported operator '%=' for type %v", right.Type())
 	}
 	if right.(*obj.Integer).Val() == 0 {
 		return obj.NewError("Can't divide by 0")

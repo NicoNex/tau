d4b1aa7c73b7a7afd86584603ad67782cce9ef5b
Nicolò Santamaria <nicolo.santamaria@protonmail.com>
2022-07-31 12:52:43 +0200

    Make REPL multiline for Windows.

--- repl_win.go
+++ repl_win.go
@@ -7,12 +7,15 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/NicoNex/tau/compiler"
 	"github.com/NicoNex/tau/obj"
 	"github.com/NicoNex/tau/parser"
 	"github.com/NicoNex/tau/vm"
 )
+
+const prompt = ">>> "
 
 func evalREPL() {
 	var (
@@ -21,11 +24,13 @@ func evalREPL() {
 	)
 
 	for {
-		fmt.Print(">>> ")
+		fmt.Print(prompt)
 		input, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println(err)
-			return
+		check(err)
+
+		if len(input) > 0 && input[len(input)-1] == '{' {
+			input, err = acceptUntil(reader, input, "\n\n")
+			check(err)
 		}
 
 		res, errs := parser.Parse(input)
@@ -55,11 +60,13 @@ func vmREPL() {
 	}
 
 	for {
-		fmt.Print(">>> ")
+		fmt.Print(prompt)
 		input, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Println(err)
-			return
+		check(err)
+
+		if len(input) > 0 && input[len(input)-1] == '{' {
+			input, err = acceptUntil(reader, input, "\n\n")
+			check(err)
 		}
 
 		res, errs := parser.Parse(input)
@@ -84,4 +91,34 @@ func vmREPL() {
 
 		fmt.Println(tvm.LastPoppedStackElem())
 	}
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+}
+
+func acceptUntil(r *bufio.Reader, start, end string) (string, error) {
+	var buf strings.Builder
+
+	buf.WriteString(start)
+	buf.WriteRune('\n')
+	for {
+		fmt.Print(prompt)
+		line, err := r.ReadString('\n')
+		if err != nil {
+			return "", err
+		}
+
+		buf.WriteString(line)
+		buf.WriteRune('\n')
+
+		if s := buf.String(); len(s) > len(end) && s[len(s)-len(end):] == end {
+			break
+		}
+	}
+
+	return buf.String(), nil
 }

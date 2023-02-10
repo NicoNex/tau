f0475063ef15a22f7d5cd530d4fdd5573a7f5a21
Nicolò Santamaria <nicolo.santamaria@protonmail.com>
2023-02-10 15:49:06 +0100

    Disable the evaluator.
    It will continue to be used under the hood for some compiler
    optimisations.

--- cmd/tau/main.go
+++ cmd/tau/main.go
@@ -10,14 +10,12 @@ import (
 func main() {
 	var (
 		compile bool
-		useEval bool
 		useFast bool
 		version bool
 		simple  bool
 	)
 
 	flag.BoolVar(&useFast, "f", false, "Use the fast implementation Tau VM (alpha).")
-	flag.BoolVar(&useEval, "eval", false, "Use the Tau eval function instead of the Tau VM. (slower)")
 	flag.BoolVar(&compile, "c", false, "Compile a tau file into a '.tauc' bytecode file.")
 	flag.BoolVar(&simple, "s", false, "Use simple REPL instead of opening a terminal.")
 	flag.BoolVar(&version, "v", false, "Print Tau version information.")
@@ -31,26 +29,16 @@ func main() {
 		tau.PrintVersionInfo(os.Stdout)
 
 	case flag.NArg() > 0:
-		if useEval {
-			tau.ExecFileEval(flag.Arg(0))
-		} else if useFast {
+		if useFast {
 			tau.ExecFileFastVM(flag.Arg(0))
 		} else {
 			tau.ExecFileVM(flag.Arg(0))
 		}
 
 	case simple:
-		if useEval {
-			tau.SimpleEvalREPL()
-		} else {
-			tau.SimpleVmREPL()
-		}
+		tau.SimpleVmREPL()
 
 	default:
-		if useEval {
-			tau.EvalREPL()
-		} else {
-			tau.VmREPL()
-		}
+		tau.VmREPL()
 	}
 }
